51634e4a2b4972362b6ce367b0be5dd968d0528e
sumit kumar <sumitkmr62@gmail.com>
2026-07-11 03:06:03 +0530

    Initial Project Structure

---
+++ Scripts/00_Setup/001_CodingStandards.sql
@@ -0,0 +1,15 @@
+/******************************************************************************
+SQL Coding Standards
+
+1. PascalCase object names
+2. Singular table names
+3. Explicit constraint names
+4. Explicit index names
+5. No SELECT *
+6. Fully qualified object names
+7. One object per script
+8. Every script rerunnable where possible
+9. Consistent naming convention
+10. Professional comments
+
+******************************************************************************/
---
+++ Scripts/00_Setup/00_ProjectInformation.sql
@@ -0,0 +1,25 @@
+/**************************************************************
+Project		:	BuildingDW
+Book		:	Building a Data Warehouse with Examples in SQL Server
+Author		:	Sumit
+Platform	:	SQL Server 2022
+Purpose		:	Enterprise Data Warehouse Implementation
+
+Storage Target 
+--------------
+Maximum Database Size : Maximum 500 MB
+
+Architecture
+------------
+Landing (later) -> NDS (Normalized Data Store) -> DDS (Dimensional Data Store) -> Reporting
+
+Database Files
+--------------
+PRIMARY
+FG_NDS
+FG_DDS
+FG_INDEX
+FG_ETL
+FG_ARCHIVE
+
+
